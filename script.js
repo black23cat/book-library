2036f9e189c86ff45f769286785bf4c264eb5b08
black2car <black2car+prayogi.pangestu1299@gmail.com.noreply.github.com>
2024-10-29 20:47:47 +0700

    Add function to open/close form modal and add book constructor

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World!")
+const myLibrary = [];
+const showBtn = document.getElementById("show-form-modal");
+const formDialog = document.getElementById("form-modal");
+const submitBtn = document.getElementById("submit");
+const form = document.querySelector("form");
+const content = document.querySelector(".content");
+
+// Open form modal
+showBtn.addEventListener("click", ()=>{
+  formDialog.showModal();
+});
+
+// Close form modal
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  form.reset();
+  formDialog.close();
+});
+
+// new book constructor
+function Book(author, title, pages, read){
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+  this.bookData = kebabCase(title);
+}
