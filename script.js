8b3e7614ed375dd7ea715812da2fdce7a58058c1
black2car <black2car+prayogi.pangestu1299@gmail.com.noreply.github.com>
2024-10-30 06:10:11 +0700

    Add function to check book title and author, Add addBookToLibrary function

--- script.js
+++ script.js
@@ -26,16 +26,36 @@ function Book(author, title, pages, read){
   this.bookData = kebabCase(title);
 }
 
-function addBookToLibrary(author, title, pages, read){
+function addBookToLibrary(a, t, p, r){
   // Check if there's already a book in a myLibrary array
   if(myLibrary.length == 0){
-    myLibrary.push(new Book(author, title, pages, read));
+    myLibrary.push(new Book(a, t, p, r));
   }else{
     // if the book is already in myLibrary this func will not add the book
-    if(checkAuthor(myLibrary, author) && checkTitle(myLibrary, title)){
+    if(checkAuthor(myLibrary, a) && checkTitle(myLibrary, t)){
       alert(`This book is already in your galery.`);
     }else{
-      myLibrary.push(new Book(author, title, pages, read));
+      myLibrary.push(new Book(a, t, p, r));
     }
   };
+}
+
+function checkAuthor(array, a){
+  const bookAuthor = array.map(function(e){
+    return e.author;
+  });
+  let result;
+	bookAuthor.filter(e =>
+    e === a ? result = true: result = false);
+  return result;
+}
+
+function checkTitle(array, t){
+  const bookAuthor = array.map(function(e){
+    return e.title;
+  });
+  let result;
+	bookAuthor.filter(e =>
+    e === t ? result = true: result = false);
+  return result;
 }
